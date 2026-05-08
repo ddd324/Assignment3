0b2508c5c73fcc5033d3a0bc7e26407ad2fbd515
pollyyy123 <731313808@qq.com>
2026-05-08 22:07:40 +1000

    initialization

--- File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Djy on 24/04/2026.
-//huiyi8yi8i
-
-import Foundation
---
+++ SnapRead/SnapRead/SnapReadApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct SnapReadApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+
+        }
+    }
+}
